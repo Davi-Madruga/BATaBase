79b75762e9abd1b013b3711c20b5497c4b617f88
F3IT3 <davimadruga@outlook.com.br>
2025-11-08 15:08:14 -0300

    FEAT STRUCTURE

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <h1>Titulo</h1>
+    <?php
+    echo"<p>Texto teste para testar o teste</p>";
+    ?>
+</body>
+</html>
