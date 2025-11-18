d717b7203606824e69e0c4f13b454f332e8c1a49
Davi-Madruga <davimadruga@outlook.com.br>
2025-11-18 16:19:01 -0300

    STYLE CSS tela.php

--- navegacao/tela.php
+++ navegacao/tela.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BATaBase</title>
+    <title>BATABASE</title>
     <link rel="stylesheet" href="../css/tela.css">
     <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon">
-</head>
-<body>
+<link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
+
+    <body>
     <a href="../index.php"><button class="BtoFechar">X</button></a>
-    <a href="itens/itens.php"><button id="itens">Itens</button></a>
-    <a href="viloes/viloes.php"><button id="viloes">Viloes</button></a>
-    <button id="veiculos">Veiculos</button>
-    <button id="trajes">trajes</button>
+    <h1 class="titulo">BATABASE</h1>
+    <div class="menu">
+        <a href="itens/itens.php"><button id="itens" class="botoes">ITENS</button></a>
+        <a href="viloes/viloes.php"><button id="viloes" class="botoes">VILÕES</button></a>
+        <button id="veiculos" class="botoes">VEICULOS</button>
+        <button id="trajes" class="botoes">TRAJES</button>
+    </div>
 </body>
 </html>
